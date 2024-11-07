269812973c6dedbf9755d180d10664eaeea260f8
Ashikur Rahman <ashikur35-540@diu.edu.bd>
2024-11-07 20:38:15 +0600

    initialzed repo and created the base UI

---
+++ src/components/icons/IconStackFill.tsx
@@ -0,0 +1,19 @@
+// icon:stack-fill | Remix Icon https://remixicon.com/ | Remix Design
+import * as React from "react";
+
+function IconStackFill(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path fill="none" d="M0 0h24v24H0z" />
+      <path d="M20.083 10.5l1.202.721a.5.5 0 010 .858L12 17.65l-9.285-5.571a.5.5 0 010-.858l1.202-.721L12 15.35l8.083-4.85zm0 4.7l1.202.721a.5.5 0 010 .858l-8.77 5.262a1 1 0 01-1.03 0l-8.77-5.262a.5.5 0 010-.858l1.202-.721L12 20.05l8.083-4.85zM12.514 1.309l8.771 5.262a.5.5 0 010 .858L12 13 2.715 7.429a.5.5 0 010-.858l8.77-5.262a1 1 0 011.03 0z" />
+    </svg>
+  );
+}
+
+export default IconStackFill;
