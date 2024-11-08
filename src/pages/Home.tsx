85cefd508fc297705619c56f3a683e991864ee93
Ashikur Rahman <ashikur35-540@diu.edu.bd>
2024-11-08 17:21:46 +0600

    feat: Added loader while fetching data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,19 +8,10 @@ import { TodoType } from "../types";
 
 const KanbanBoard = () => {
   const [todos, setTodos] = useState<TodoType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // process the data from the API
-  /*
-export const kanbanData = [
-  { title: "Incomplete", items: Array(30).fill(todo) },
-  { title: "To Do", items: Array(30).fill(todo) },
-  { title: "Doing", items: Array(30).fill(todo) },
-  { title: "Under Review", items: Array(30).fill(todo) },
-  { title: "Completed", items: Array(30).fill(todo) },
-  { title: "Overdue", items: Array(30).fill(todo) },
-];
 
-*/
   const todoStatusArr = [...new Set(todos && todos.map((todo) => todo.status))];
   const kanbanData = todoStatusArr.map((status) => ({
     title: status,
@@ -28,32 +19,128 @@ export const kanbanData = [
     total: todos.filter((todo) => todo.status === status).length,
   }));
   useEffect(() => {
+    setIsLoading(true);
     getAllTodos()
       .then((data) => {
         // console.log("✨ ~ file: Home.tsx:14 ~ .then ~ data:", data);
         setTodos(data);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.log(
           "✨ ~ file: KanbanBoard.tsx:24 ~ getAllTodos ~ error",
           error
         );
+        setIsLoading(false);
       });
   }, []);
   return (
     <section className="p-4">
-      <div className="mb-4">
-        <TodoManagerModal modalType="add" />
-      </div>
-      <div className="flex  space-x-4 ">
-        {kanbanData.map((column, index) => (
-          <KanbanColumn key={index} column={column}>
-            {column.todo.map((item, idx) => (
-              <KanbanCard todo={item} key={idx} />
+      {isLoading ? (
+        <div className="flex h-screen justify-center items-center">
+          <div
+            aria-label="Loading..."
+            role="status"
+            className="flex items-center space-x-2"
+          >
+            <svg
+              className="h-20 w-20 animate-spin stroke-gray-500"
+              viewBox="0 0 256 256"
+            >
+              <line
+                x1="128"
+                y1="32"
+                x2="128"
+                y2="64"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="24"
+              ></line>
+              <line
+                x1="195.9"
+                y1="60.1"
+                x2="173.3"
+                y2="82.7"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="24"
+              ></line>
+              <line
+                x1="224"
+                y1="128"
+                x2="192"
+                y2="128"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="24"
+              ></line>
+              <line
+                x1="195.9"
+                y1="195.9"
+                x2="173.3"
+                y2="173.3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="24"
+              ></line>
+              <line
+                x1="128"
+                y1="224"
+                x2="128"
+                y2="192"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="24"
+              ></line>
+              <line
+                x1="60.1"
+                y1="195.9"
+                x2="82.7"
+                y2="173.3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="24"
+              ></line>
+              <line
+                x1="32"
+                y1="128"
+                x2="64"
+                y2="128"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="24"
+              ></line>
+              <line
+                x1="60.1"
+                y1="60.1"
+                x2="82.7"
+                y2="82.7"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="24"
+              ></line>
+            </svg>
+            <span className="text-4xl font-medium text-gray-500">
+              Loading...
+            </span>
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className="mb-4">
+            <TodoManagerModal modalType="add" />
+          </div>
+          <div className="flex  space-x-4 ">
+            {kanbanData.map((column, index) => (
+              <KanbanColumn key={index} column={column}>
+                {column.todo.map((item, idx) => (
+                  <KanbanCard todo={item} key={idx} />
+                ))}
+              </KanbanColumn>
             ))}
-          </KanbanColumn>
-        ))}
-      </div>
+          </div>
+        </>
+      )}
     </section>
   );
 };
